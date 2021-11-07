cf1b89f8ec9ba4154e87d979f170813f9d25c530
jsopdebeeck <47308114+jsopdebeeck@users.noreply.github.com>
2021-11-07 16:22:11 +0100

    Update do.sh

--- do.sh
+++ do.sh
@@ -1,3 +1,10 @@
+# Basic stuffs
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt autoremove
+
+# Source page https://docs.tenable.com/nessus/10_0/Content/DownloadNessus.htm 
 
 dpkg -i Nessus-10.0.0-raspberrypios_armhf.deb
                                                             
